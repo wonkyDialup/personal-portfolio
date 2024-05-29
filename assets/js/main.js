f945c634005c6419607056bd5a444fdc08dfb35f
Justin Gibbs <justin.gibbs59@gmail.com>
2024-05-29 18:43:45 -0400

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,7 @@
    */
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
-    let position = window.scrollY + 201
+    let position = window.scrollY + 200
     navbarlinks.forEach(navbarlink => {
       if (!navbarlink.hash) return
       let section = select(navbarlink.hash)
